70e6db383748ebb4ef9e5d14197543bcbe2794a2
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-01-30 21:33:42 +0900

    delete target

--- hello/target/debug/deps/hello-7a959ef384f994c6.d
+++
@@ -1,7 +0,0 @@
-/Users/yamazakitakahiro/.config/nvim/hello/target/debug/deps/hello-7a959ef384f994c6.rmeta: src/main.rs
-
-/Users/yamazakitakahiro/.config/nvim/hello/target/debug/deps/hello-7a959ef384f994c6.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:PATH=/Users/yamazakitakahiro/.cargo/bin:/Users/yamazakitakahiro/.cargo/bin:/Users/yamazakitakahiro/.cargo/bin/:/Users/yamazakitakahiro/go/bin:/Users/yamazakitakahiro/.nodebrew/current/bin:/Users/yamazakitakahiro/.flutterbin/bin:/Users/yamazakitakahiro/.rbenv/shims:/usr/local/Cellar/pyenv-virtualenv/1.1.5/shims:/Users/yamazakitakahiro/.pyenv/shims:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin
--- hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/Users/yamazakitakahiro/.config/nvim/hello/target/debug/hello: /Users/yamazakitakahiro/.config/nvim/hello/src/main.rs
