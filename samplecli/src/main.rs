13e39a4576a3a1f044085a40af0f49e4169786fd
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-02-07 00:26:33 +0900

    until 4-1

---
+++ samplecli/src/main.rs
@@ -0,0 +1,25 @@
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "kaepa3",
+    about = "Supre Calculator"
+)]
+
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+fn main() {
+    let opts = Opts::parse();
+
+    match opts.formula_file{
+        Some(file) => println!("File specified:{}", file),
+        None => println!("No file"),
+    }
+    println!("Is verbosity specified?:{}", opts.verbose);
+}
